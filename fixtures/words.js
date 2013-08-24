1daff041554954709b3b6b691827022b1f92dca9
PsyBurn <kresimir.antolic@infinum.hr>
2013-08-24 13:04:50 +0200

    word [].

--- fixtures/words.js
+++ fixtures/words.js
@@ -1,6 +1,6 @@
 define(['can/util/string', 'can/util/fixture'], function(can){
 
-  var words = ['abandon','abandoned','ability','able','about','above','abroad','absence','absent','absolute','absolutely','absorb','abuse','academic','accent','acceptable','accept','access','accident','accidental','accommodation','accompany','according', 'to','account','accurate','accuse','achieve','achievement','acid','acknowledge','acquire','across','act','action','active','activity','actor','actress','actual','actually','ad','adapt','add','addition','additional','address','adequate','adjust','admiration','admire','admit','adopt','adult','advance', 'anal (as requested by @gperetin)']
+  var words = ['a[band]on','ab[and]oned','ab[ili]ty','a[b]le','abo[ut]','a[b]ov[e]','[a]broad','ab[sence]','[ab]sent','ab[solute]','[a]bsolut[e]ly','[abs]orb','[abuse]','a[c]ademi[c]','a[cc]ent','[ac]c[e]ptable','[acc]ept','[acc]ess','acci[dent]','Acc[i]dental','[acc]ommo[da]tion','ac[company]','[acc]ording','ac[count]','[acc]u[rate]','ac[c]use','a[chi]eve','ach[ie]vement','aci[d]','[ack]nowledge','ac[q]ui[r]e','acro[ss]','a[ct]ion','[a]ct[i]v[e]','activ[ity]','[a]ctor','act[ress]','a[ct]ual','actually','ad[ap]t','[add]','a[dd]ition','additio[nal]','[add]ress','[a]deq[uat]e','ad[j]ust','[adm]irat[ion]','[a]dm[i]r[e]','adm[it]','ad[o]pt','adul[t]','a[dv]a[nce]', '[ana]l (as requested by @gperetin)']
 
 
   var store = can.fixture.store(words.length - 1, function(i){
