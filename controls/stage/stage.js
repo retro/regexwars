a03b5fc6cdcdfc0690e2b18a882fff3a5ec63fb9
Mihael Konjevic <konjevic@gmail.com>
2013-08-24 14:47:52 +0200

    Adding bullets to the stage

--- controls/stage/stage.js
+++ controls/stage/stage.js
@@ -1,4 +1,4 @@
-define(['can/util/string', 'mustache!./init', 'can/control', 'less!./stage'], function(can, initView){
+define(['can/util/string', 'mustache!./init', 'models/bullet', 'can/control', 'less!./stage'], function(can, initView, BulletModel){
 
 	return can.Control({
 		defaults : {
@@ -11,11 +11,15 @@ define(['can/util/string', 'mustache!./init', 'can/control', 'less!./stage'], fu
 			this.areaHeight = can.compute(0);
 			this.areaWidth  = can.compute(0);
 
+			this.bullets    = new BulletModel.List;
+
+			this.bulletOrigin = {x: 0, y: 0};
 
 			this.mousePosition = can.compute({
 				x: 0,
 				y: 0
 			});
+
 			this.angle = can.compute(function(){
 				var position   = self.mousePosition(),
 					center     = (self.areaWidth() / 2),
@@ -23,26 +27,51 @@ define(['can/util/string', 'mustache!./init', 'can/control', 'less!./stage'], fu
 					deltaY     = self.areaHeight() - position.y,
 					deltaX     = center - position.x,
 					angle      = -Math.atan(deltaX / deltaY) * 90;
-				return Math.min(Math.max(-67, angle), 67)
-			})
-			
+
+				return Math.min(Math.max(-67, angle), 67);
+			});
+
 			this.element.html(initView({
-				angle : this.angle
+				angle   : this.angle,
+				bullets : this.bullets
 			}));
 
 			this.$area  = this.element.find('#area');
+			this.$gun   = this.element.find('#gun');
 
 			this.offset = this.$area.offset();
 
 			this.areaWidth(this.$area.width());
 			this.areaHeight(this.$area.height());
 			
+			window.requestAnimationFrame(this.bullets.proxy('move'));
 		},
 		"#area mousemove" : function(el, ev){
+			var position = this.$gun.position()
 			this.mousePosition({
 				x: ev.pageX - this.offset.left,
 				y: ev.pageY - this.offset.top
-			})
+			});
+
+			this.bulletOrigin.x = position.left;
+			this.bulletOrigin.y = position.top;
+		},
+		"#area click" : function(){
+			var leftPos = this.bulletOrigin.x,
+				angle   = this.angle(),
+				boundingRect;
+
+			if(angle > 0){
+				boundingRect = this.$gun[0].getBoundingClientRect();
+				leftPos = boundingRect.right;
+			}
+
+			this.bullets.push(new BulletModel({
+				regex : 'foo',
+				top   : this.bulletOrigin.y,
+				left  : leftPos,
+				angle : angle
+			}))
 		}
 	});
 
--- models/bullet.js
+++ models/bullet.js
@@ -1,6 +1,6 @@
-define(['can/util/string', 'can/model'], function(can){
+define(['can/util/string', 'can/model', 'can/construct/proxy'], function(can){
 
-	return can.Model({
+	var Model = can.Model({
 
 		findAll : 'GET /bullets',
 		findOne : 'GET /bullets/{id}',
@@ -9,7 +9,19 @@ define(['can/util/string', 'can/model'], function(can){
 		destroy : 'DELETE /bullets/{id}'
 
 	}, {
-		
+		move : function(){
+			
+		}
 	});
+
+	Model.List = can.Model.List({
+		move : function(){
+			this.map(function(b){
+				b.move();
+			})
+		}
+	})
+
+	return Model;
 
 })
