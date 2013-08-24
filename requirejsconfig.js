33e2009cb8471e10fe8ad219ff68f42e260b3deb
Mihael Konjevic <konjevic@gmail.com>
2013-08-24 12:35:07 +0200

    Add bower components

--- requirejsconfig.js
+++ requirejsconfig.js
@@ -1,8 +1,18 @@
 requirejs.config({
-  paths: {
-    can      : 'bower_components/canjs/amd/can',
-    jquery   : 'bower_components/jquery/jquery',
-    mustache : 'bower_components/require-can-renderers/lib/mustache',
-    ejs      : 'bower_components/require-can-renderers/lib/ejs'
-  }
+	paths: {
+		can      : 'bower_components/canjs/amd/can',
+		jquery   : 'bower_components/jquery/jquery',
+		mustache : 'bower_components/require-can-renderers/lib/mustache',
+		ejs      : 'bower_components/require-can-renderers/lib/ejs'
+	},
+	packages : [{
+		name : 'css',
+		location: 'components/require-css',
+		main : 'css'
+	},
+	{
+		name : 'less',
+		location: 'components/require-less',
+		main : 'less'
+	}]
 });
